1a07eb612b237c8035aeb05693f7bfe906470dcc
simonZweimueller <simon.zweimueller1@gmail.com>
2020-11-15 12:22:21 +0100

    initial commit

---
+++ rest-kotlin-quickstart/src/main/kotlin/at/htl/rest/GreetingService.kt
@@ -0,0 +1,11 @@
+package at.htl.rest
+
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class GreetingService {
+
+    fun greeting(name: String): String {
+        return "hello $name"
+    }
+}
